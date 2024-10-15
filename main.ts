6bc7cb92f33991e68225a3370481ae11fa03ebea
Aditya Kumar Sahu <145532705+adityasahusentra@users.noreply.github.com>
2024-10-15 13:16:56 +0530

    working chat app

--- main.ts
+++ main.ts
@@ -4,10 +4,24 @@
 /// <reference lib="dom.asynciterable" />
 /// <reference lib="deno.ns" />
 
-import "$std/dotenv/load.ts";
+import { Application, Context, Router } from "@oak/oak";
+import ChatServer from "./ChatServer.ts";
 
-import { start } from "$fresh/server.ts";
-import manifest from "./fresh.gen.ts";
-import config from "./fresh.config.ts";
+const app = new Application();
+const port = 8080;
+const router = new Router();
+const server = new ChatServer();
 
-await start(manifest, config);
+router.get("/start_web_socket", (ctx: Context) => server.handleConnection(ctx));
+
+app.use(router.routes());
+app.use(router.allowedMethods());
+app.use(async (context) => {
+    await context.send({
+        root: Deno.cwd(),
+        index: "public/index.html",
+    });
+});
+
+console.log("Listening at http://localhost:" + port);
+await app.listen({ port });
